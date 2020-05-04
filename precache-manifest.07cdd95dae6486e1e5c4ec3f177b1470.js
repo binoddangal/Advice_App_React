ac0e99f6dd690af3ec5651353efaa85220a5790d
binoddangal <dangalbinod555@gmail.com>
2020-05-04 15:49:40 +0545

    Updates

---
+++ precache-manifest.07cdd95dae6486e1e5c4ec3f177b1470.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c3e366644564d54e73aff2ea30e6e1a",
+    "url": "/Advice_App_React/index.html"
+  },
+  {
+    "revision": "9ae8c595b1c94bf80ef6",
+    "url": "/Advice_App_React/static/css/main.ef19d89f.chunk.css"
+  },
+  {
+    "revision": "1452e7a29c19c546dacf",
+    "url": "/Advice_App_React/static/js/2.329e1757.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Advice_App_React/static/js/2.329e1757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ae8c595b1c94bf80ef6",
+    "url": "/Advice_App_React/static/js/main.5bfb8c36.chunk.js"
+  },
+  {
+    "revision": "e73901b2b7a491d9bea8",
+    "url": "/Advice_App_React/static/js/runtime-main.5af585f5.js"
+  },
+  {
+    "revision": "906d9227a8defdb4ddde8f5101385d63",
+    "url": "/Advice_App_React/static/media/img.906d9227.jpg"
+  }
+]);
